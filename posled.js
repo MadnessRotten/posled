b754017a784320879d55e1b08f27d7510fa7a839
MadnessRotten <136122155+MadnessRotten@users.noreply.github.com>
2023-06-11 22:19:28 +0200

    Add files via upload

---
+++ posled.js
@@ -0,0 +1,37 @@
+const findLCS = (str1, str2) => {
+    const m = str1.length;
+    const n = str2.length;
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+  
+    for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+        if (str1[i - 1] === str2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+  
+    let lcs = '';
+    let i = m, j = n;
+    while (i > 0 && j > 0) {
+      if (str1[i - 1] === str2[j - 1]) {
+        lcs = str1[i - 1] + lcs;
+        i--;
+        j--;
+      } else if (dp[i - 1][j] > dp[i][j - 1]) {
+        i--;
+      } else {
+        j--;
+      }
+    }
+  
+    return lcs;
+  };
+  
+  const string1 = 'ABCDGH';
+  const string2 = 'AEDFHR';
+  const lcs = findLCS(string1, string2);
+  console.log(`Наибольшая общая подпоследовательность: ${lcs}`);
+  
